502dc09e3d853c983861390c28d8c46f476822ec
Mikdhad.k <mikdhadk32@gmail.com>
2024-06-24 09:45:05 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex flex-col bg-gray-900 min-h-screen p-5">
+        <p className="text-white">Yoou</p>
+        <p className="text-white">yii</p>
+        <Button className="mt-5">hi</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
